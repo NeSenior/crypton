85f394a26bf49f7ff6187ccb9ef9c20b561e10d5
NeSenior <264385066+NeSenior@users.noreply.github.com>
2026-03-02 21:12:53 +0300

    miniUp

--- crypton.cpp
+++ crypton.cpp
@@ -1,8 +1,13 @@
 #include "crypton.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <GL/gl.h>
+#include <algorithm>
 
 static GLFWwindow* window = nullptr;
+static float C_R = 1.0f;
+static float C_G = 1.0f;
+static float C_B = 0.0f;
 
 namespace crypton
 {
@@ -14,14 +19,28 @@ namespace crypton
             return;
         }
         window = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (!window)
+        {
+            std::cout << "Window creation failed\n";
+            glfwTerminate();
+            return;
+        }
+        
         glfwMakeContextCurrent(window);
         Start();
         while (!glfwWindowShouldClose(window))
         {
             glfwPollEvents();
             Update();
+            glClearColor(C_R,C_G,C_B,1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
             glfwSwapBuffers(window);
         }
         glfwTerminate();
+    }
+    void bg_color(float r,float g,float b){
+        C_R = std::clamp(r,0.0f,1.0f);
+        C_G = std::clamp(g,0.0f,1.0f);
+        C_B = std::clamp(b,0.0f,1.0f);
     }
 }
--- crypton.h
+++ crypton.h
@@ -2,6 +2,7 @@
 
 namespace crypton{
     void create(int width, int height, const char* name);
+    void bg_color(float r,float g,float b);
 }
 void Start();
 void Update();
