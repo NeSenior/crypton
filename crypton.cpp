be5d075044ba4e61dcd1b982e544e198cd5b2b59
NeSenior <daniilisaev0010@gmail.com>
2026-03-02 12:19:33 +0300

    codeCommit_02.03.26

---
+++ crypton.cpp
@@ -0,0 +1,39 @@
+#include "crypton.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+static GLFWwindow* window = nullptr;
+static float clearR = 0.0f;
+static float clearG = 0.0f;
+static float clearB = 0.0f;
+
+void create(int width, int height, const char* name)
+{
+    glfwInit();
+    window = glfwCreateWindow(width, height, name, NULL, NULL);
+    glfwMakeContextCurrent(window);
+}
+
+void setBackGroundColor(float r, float g, float b){
+    clearR = r;
+    clearG = g;
+    clearB = b;
+}
+
+void run(){
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(clearR,clearG,clearB,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        Update();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+}
+void destroy()
+{
+    glfwTerminate();
+}
---
+++ crypton.h
@@ -0,0 +1,10 @@
+#ifndef CRYPTON_H
+#define CRYPTON_H
+
+void create(int width, int height, const char* name);
+void Update();
+void run();
+void destroy();
+void setBackGroundColor(float r,float g,float b);
+
+#endif
